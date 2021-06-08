5f7b210c4c2ddea33e33e314e9f14b5ed1155bbb
Romain André-Lovichi <romain.andrelovichi@gmail.com>
2021-06-08 21:53:48 +0200

    Init with current Trolls & Castles files

---
+++ __init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*-coding:utf-8 -*
+
+from trollsEtChateaux.entities import Classement, Partie, Strategie
+from trollsEtChateaux.core import jouerPartie, jouerPlusieursParties
+from trollsEtChateaux.strategies import strategieExemple1, strategieExemple2, strategieExemple3
---
+++ entities/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*-coding:utf-8 -*
+
+from trollsEtChateaux.entities.BilanPartie import BilanPartie
+from trollsEtChateaux.entities.Classement import Classement
+from trollsEtChateaux.entities.CoupInvalideDroite import CoupInvalideDroite
+from trollsEtChateaux.entities.CoupInvalideGauche import CoupInvalideGauche
+from trollsEtChateaux.entities.CoupInvalideSimultane import CoupInvalideSimultane
+from trollsEtChateaux.entities.Partie import Partie
+from trollsEtChateaux.entities.ResultatSimulation import ResultatSimulation
+from trollsEtChateaux.entities.Strategie import Strategie
